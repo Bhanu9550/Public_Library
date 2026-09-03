731ac2447e3c0e2098ba33b3e3247a2df28e8c94
Bhanu <prakashbhanu9550@gmail.com>
2026-09-04 01:07:55 +0530

    change db credentials

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();    
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-const cors = require("cors")
-app.use(cors())
+const cors = require("cors");
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}));
 
 const { connectDB } = require("./config/db");
 
@@ -25,8 +27,8 @@ app.use("/borrow", bookController)
 const startServer = async ()=>{
     try{
         await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on port ${PORT}`);
         });
     } catch (error) {
         console.error("Error starting the server:", error);
